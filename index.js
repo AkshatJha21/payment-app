1313de1ba6361ff481f722c71b73cdcd4b5967e8
AkshatJha21 <akshatj2134@gmail.com>
2024-05-10 23:27:26 +0530

    index init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(express());
+app.use(cors());
+
+app.listen(port, (req, res) => {
+    console.log(`Backend running on port ${port}`);
+});
