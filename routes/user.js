ffe071c42cdb315d3f8b219ed638e9749fd2debd
AkshatJha21 <akshatj2134@gmail.com>
2024-05-13 10:38:48 +0530

    hash + jwt + signup done

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const { signupUser } = require("../types");
 const { User } = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
 
 const userRouter = express.Router();
 
-userRouter.post('/signup', async (req, res, next) => {
+userRouter.post('/signup', async (req, res) => {
     const newUserPayload = req.body;
     const parsedPayload = signupUser.safeParse(newUserPayload);
 
     if (!parsedPayload.success) {
-        res.status(401).json({
+        res.status(411).json({
             message: "Incorrect inputs"
         });
         return;
@@ -20,23 +23,32 @@ userRouter.post('/signup', async (req, res, next) => {
     });
 
     if (existingUser) {
-        res.status(401).json({
+        res.status(411).json({
             message: "Email already taken"
         });
         return;
     }
 
-    await User.create({
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(newUserPayload.password, salt);
+
+    const newUser = await User.create({
         email: newUserPayload.email,
-        password: newUserPayload.password,
+        password: hashedPassword,
         firstName: newUserPayload.firstName,
         lastName: newUserPayload.lastName,
     });
 
+    const userId = newUser._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
     res.status(200).json({
-        message: "User added"
+        message: "User added successfully",
+        token: token
     });
-    next();
 });
 
 module.exports = userRouter;
