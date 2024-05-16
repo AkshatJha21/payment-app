38145d7b7241b758db4cc6279e844fd2c4c52a47
AkshatJha21 <akshatj2134@gmail.com>
2024-05-16 10:02:51 +0530

    tailwind css installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,6 @@ function App() {
           <Route path='/send' element={<Transfer />}/>
         </Routes>
       </BrowserRouter>
-      asdf
     </>
   )
 }
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div>
+      <h1 className='text-blue-500'>Signup</h1>
+    </div>
   )
 }
 
