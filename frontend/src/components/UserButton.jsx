60116cd77e97daf39b6f2e566d30903e377de172
AkshatJha21 <akshatj2134@gmail.com>
2024-05-17 01:03:41 +0530

    user button done

--- frontend/src/components/UserButton.jsx
+++ frontend/src/components/UserButton.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const UserButton = () => {
+  const [user, setUser] = useState({});
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    axios.get("http://localhost:3000/api/v1/user/me" , {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+      .then(response => {
+        setUser(response.data.user);
+      })
+  }, []);
+
+  const initials = user.firstName ? user.firstName[0].toUpperCase() : '';
   return (
     <div className='flex gap-x-2 items-center'>
-        <p>Hello, User</p>
+        <p>Hello, {user.firstName}</p>
         <div className='p-2 bg-blue-300 rounded-full h-10 w-10 text-center'>
-            U
+            {initials}
         </div>
     </div>
   )
