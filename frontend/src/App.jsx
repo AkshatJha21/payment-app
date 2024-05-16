a8103007467266bd172d847ac5b710870b4ae816
AkshatJha21 <akshatj2134@gmail.com>
2024-05-16 09:32:58 +0530

    router working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Signup from './components/Signup'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Transfer from './components/Transfer';
 
 function App() {
 
   return (
     <>
-      Hello World
+      <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<Signup />}/>
+          <Route path='/signin' element={<Login />}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+          <Route path='/send' element={<Transfer />}/>
+        </Routes>
+      </BrowserRouter>
+      asdf
     </>
   )
 }
