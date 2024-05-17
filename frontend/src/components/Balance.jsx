07dd213cb34ab38272681b28d1592c88e1b0febc
AkshatJha21 <akshatj2134@gmail.com>
2024-05-17 10:49:08 +0530

    Balance fixed

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -2,32 +2,28 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 const Balance = () => {
-  const [amount, setAmount] = useState({});
+  const [account, setAccount] = useState({});
 
   useEffect(() => {
-    const getBalance = async () => {
-      try {
-        const token = localStorage.getItem('token');
-        axios.get("http://localhost:3000/api/v1/account/balance", {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        }).then(response => {
-          console.log(response.data);
-          setAmount(response.data.account);
-        });
-      } catch (error) {
-        console.error("Error fetching balance: ", error);
-      };
+    const getBalance = () => {
+      const token = localStorage.getItem('token');
+      axios.get("http://localhost:3000/api/v1/account/balance", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).then(response => {
+        setAccount(response.data.account);
+      })
+    }
 
-      getBalance();
-    };
+    getBalance();
   }, []);
+  const roundedBalance = account.balance ? parseFloat(account.balance.toFixed(3)) : 0;
 
   return (
     <div className='mx-4 mb-4 p-4'>
         <h1 className='text-lg font-semibold'>Your Balance</h1>
-        <p className='text-2xl font-bold text-blue-600'>${amount.balance}</p>
+        <p className='text-2xl font-bold text-blue-600'>${roundedBalance}</p>
     </div>
   )
 }
